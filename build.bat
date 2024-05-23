6d65c4beb14ff9a1d5816ee06e56144d07c168b4
Johannes Förstner <johannes@famfoerstner.de>
2024-05-23 21:25:00 +0200

    Build them

--- build.bat
+++ build.bat
@@ -1,8 +1,6 @@
 :: This script is supposed to be executed from your DS installation folder.
 :: TankCreator and gaspy are expected to be in sibling dirs.
 
-:: name of resources, case-sensitive
-set res_cs=ExpVoices
 :: path of DSLOA documents dir (where Bits are)
 set doc_dsloa=%USERPROFILE%\Documents\Dungeon Siege LoA
 :: path of DS installation
@@ -11,7 +9,7 @@ set ds=.
 set tc=..\TankCreator
 
 set copyright=CC-BY-SA 2024
-set title=%res_cs%
+set title=Expansion
 set author=Johannes Förstner
 
 :: param
@@ -20,13 +18,28 @@ echo %mode%
 
 :: Compile resource files
 rmdir /S /Q "%tmp%\Bits"
+robocopy "%doc_dsloa%\Bits\sound\effects" "%tmp%\Bits\sound\effects" /E
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\ExpSoundEffects.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
+if %errorlevel% neq 0 pause
+
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%doc_dsloa%\Bits\sound\music" "%tmp%\Bits\sound\music" /E
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\ExpMusic.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
+if %errorlevel% neq 0 pause
+
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%doc_dsloa%\Bits\movies" "%tmp%\Bits\movies" /E
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\ExpMovies.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
+if %errorlevel% neq 0 pause
+
+rmdir /S /Q "%tmp%\Bits"
 robocopy "%doc_dsloa%\Bits\sound\voices" "%tmp%\Bits\sound\voices" /E /xf *_ack_* /xf *_select_* /xf *_attack_*
-%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%res_cs% - dialog.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\ExpVoices - dialog.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
 if %errorlevel% neq 0 pause
 
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%doc_dsloa%\Bits\sound\voices" "%tmp%\Bits\sound\voices" /E *_ack_* *_select_* *_attack_*
-%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%res_cs% - voiceover.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\ExpVoices - voiceover.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
 if %errorlevel% neq 0 pause
 
 :: Cleanup
--- cleanup.bat
+++ cleanup.bat
@@ -1,10 +1,7 @@
 :: This script is supposed to be executed from your DS installation folder.
 
-:: name of resources, case-sensitive
-set res_cs=ExpVoices
 :: path of DS installation
 set ds=.
 
 :: Cleanup resources so as not to confuse Siege Editor
-del "%ds%\DSLOA\%res_cs% - dialog.dsres"
-del "%ds%\DSLOA\%res_cs% - voiceover.dsres"
+del "%ds%\DSLOA\Exp*.dsres"
