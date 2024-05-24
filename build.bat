533e46c5c4376942674fa566abb0ad5be3b4adf8
Johannes Förstner <johannes@famfoerstner.de>
2024-05-24 20:48:33 +0200

    Add decals to terrain build

--- build.bat
+++ build.bat
@@ -38,6 +38,7 @@ if %errorlevel% neq 0 pause
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%doc_dsloa%\Bits\art\terrain" "%tmp%\Bits\art\terrain" /E
 robocopy "%doc_dsloa%\Bits\art\bitmaps\terrain" "%tmp%\Bits\art\bitmaps\terrain" /E
+robocopy "%doc_dsloa%\Bits\art\bitmaps\decals" "%tmp%\Bits\art\bitmaps\decals" /E
 robocopy "%doc_dsloa%\Bits\art" "%tmp%\Bits\art" /E *terrain*.nnk
 %tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\ExpTerrain.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
 if %errorlevel% neq 0 pause
